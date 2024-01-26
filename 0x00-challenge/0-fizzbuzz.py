5e30977a886f35fd16e1adf25ca915ebb81ddde5
mahmoudbalbaa <ma7moud.balbaa@gmail.com>
2024-01-26 05:49:30 -0800

    tsk 0 update

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ FizzBuzz
 """
+
 import sys
 
-
-def fizzbuzz(n):
+def fizz_buzz(n):
     """
     FizzBuzz function prints numbers from 1 to n separated by a space.
 
@@ -15,25 +15,24 @@ def fizzbuzz(n):
     if n < 1:
         return
 
-    tmp_result = []
+    result = []
     for i in range(1, n + 1):
-        if (i % 3) == 0 and (i % 5) == 0:
-            tmp_result.append("FizzBuzz")
-        elif (i % 3) == 0:
-            tmp_result.append("Fizz")
-        elif (i % 5) == 0:
-            tmp_result.append("Buzz")
+        if i % 3 == 0 and i % 5 == 0:
+            result.append("FizzBuzz")
+        elif i % 3 == 0:
+            result.append("Fizz")
+        elif i % 5 == 0:
+            result.append("Buzz")
         else:
-            tmp_result.append(str(i))
-    print(" ".join(tmp_result))
-
+            result.append(str(i))
+    print(" ".join(result))
 
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
         print("Missing number")
-        print("Usage: ./0-fizzbuzz.py <number>")
-        print("Example: ./0-fizzbuzz.py 89")
+        print("Usage: ./fizzbuzz.py <number>")
+        print("Example: ./fizzbuzz.py 89")
         sys.exit(1)
 
     number = int(sys.argv[1])
-    fizzbuzz(number)
+    fizz_buzz(number)
